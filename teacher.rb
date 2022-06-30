8fa721e1c627ba31a4c513b80db22aad7f236c17
sigeikiprono4@gmail.com <sigeikiprono4@gmail.com>
2022-06-30 17:39:42 +0300

    Solve rubocop issues

--- teacher.rb
+++ teacher.rb
@@ -3,8 +3,8 @@ require_relative 'person'
 class Teacher < Person
   attr_accessor :specialization
 
-  def initialize(name = "Unknown", age = 0, parent_permission = true, specialization = "Unknown")
-    super(name, age, parent_permission)
+  def initialize(name = 'Unknown', specialization = 'Unknown', parent_permission: true)
+    super(name, parent_permission)
     @specialization = specialization
   end
 
